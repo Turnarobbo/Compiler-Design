7cf7671329017425e0ea12bf377667a2237fe7e0
TurnaFahmida <145374355+Turnarobbo@users.noreply.github.com>
2023-09-24 23:35:58 +0600

    Add files via upload

--- operator1.cpp
+++ operator1.cpp
@@ -1,48 +1,19 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+
 using namespace std;
+
 int main() {
-    string a;
-    cout << "Please enter any equation you want";
-    cin >>a;
+    string input;
 
-    bool hasPlus = false;
-    bool hasEqual = false;
-    bool hasminus = false;
-    bool hasdevie= false;
-    bool hasparsent= false;
-    bool hasmultiplication= false;
-    for (char c : a) {
-        if (c == '+'||c=='-'||c=='*'||c=='/'||c=='*'||c=='%'||c=='=') {
-            hasPlus = true;
-            hasparsent=true;
-            hasmultiplication=true;
-            hasminus=true;
-            hasEqual=true;
-            hasdevie=true;
-        } else if (c == '+'||c=='-'||c=='*'||c=='/'||c=='*'||c=='%'||c=='=') {
-            hasPlus = true;
-            hasparsent=true;
-            hasmultiplication=true;
-            hasminus=true;
-            hasEqual=true;
-            hasdevie=true;
+    cout << "Enter a string: ";
+    cin >> input;
+
+    for (char c : input) {
+        if (c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '=') {
+            cout << "Operator: " << c << endl;
         }
-
-    }
-
-    if (hasPlus && hasEqual && hasdevie &&hasminus&&hasmultiplication && hasparsent) {
-        cout << "There is an operation"<<endl;
-    }
-    else if (hasPlus && hasEqual && hasdevie &&hasminus&&hasmultiplication && hasparsent) {
-        cout << "The input contains the two or more operator." << endl;
-    }
-    else {
-        cout << "There is no operation" << endl;
     }
 
     return 0;
 }
-
-
-
